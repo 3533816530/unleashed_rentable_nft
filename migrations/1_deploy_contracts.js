2160a5a39c59a53f3aca2b3e6096103abd57c428
3533816530 <132976993+3533816530@users.noreply.github.com>
2024-10-19 15:39:54 +0800

    Update 1_deploy_contracts.js

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,5 +1,8 @@
-const RentablePets = artifacts.require("RentablePets");
+// artifacts 是由 Truffle 提供的一个全局对象，允许我们引入编译后的智能合约文件。
+// 它将编译器生成的 ABI（Application Binary Interface，应用二进制接口）和字节码与 JavaScript 代码连接起来。
+const RentablePets = artifacts.require("RentablePets"); // 引入名为 RentablePets 的智能合约。Truffle 会寻找一个名为 RentablePets 的合约，并将其映射到 JavaScript 对象中供后续部署时使用。
 
+// 当运行 truffle migrate 或 truffle deploy 时，Truffle 会自动执行这个文件，并按照这里定义的步骤来部署合约。
 module.exports = function (deployer) {
-  deployer.deploy(RentablePets);
+  deployer.deploy(RentablePets);// 部署一个智能合约到区块链上
 };
