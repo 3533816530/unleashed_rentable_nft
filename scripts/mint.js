1218daecba1b17becd669370b24ffc4bb835f047
3533816530 <132976993+3533816530@users.noreply.github.com>
2024-10-19 15:44:46 +0800

    Update mint.js

--- scripts/mint.js
+++ scripts/mint.js
@@ -4,9 +4,11 @@ const main = async (cb) => {
     try {
         const argv = require('yargs/yargs')(process.argv.slice(4))
             .default("from", (await web3.eth.getAccounts())[0])
-            .argv;
+            .argv; // 使用yargs库进行参数解析
         const rentablePets = await RentablePets.deployed()
-        let txn = await rentablePets.mint("fakeURI", {from: argv.from})
+        let txn = await rentablePets.mint("fakeURI", {from: argv.from}) // 调用 RentablePets 合约的 mint 方法，传入一个字符串 "fakeURI" 作为 NFT 的 URI，
+                                                                        // 并指定 from 参数作为发送交易的账户。
+                                                                        // 这个调用将返回一个交易对象（txn），该对象包含关于交易的信息，例如交易哈希、区块号等。
         console.log(txn);
     } catch(err) {
         console.log(err);
